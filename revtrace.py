89d8a6fc6dec53fd1fa54fb383df86e0909da6c9
dbr <dbr.onix@gmail.com>
2008-10-12 20:11:02 +1030

    Added more complex example to revtrace test

--- revtrace.py
+++ revtrace.py
@@ -4,9 +4,18 @@ Demonstrates determining which functions a command depends on.
 May use this to intellimigentalylly determine which tests to
 run when code changes.
 """
+class hmmm:
+    def __init__(self):
+        self._blah()
+    def _blah(self):
+        something()
 
 def mystart():
+    x = hmmm()
     print subcall("Yay!")
+
+def something():
+    subcall("hi")
 
 def subcall(hmmm):
     return hmmm * 2
