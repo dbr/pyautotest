727e2456057ee67c9d3dc5d182a39c5ee577b0d1
dbr <dbr.onix@gmail.com>
2008-10-12 20:04:46 +1030

    Example tests

--- thine_tests.py
+++ thine_tests.py
@@ -3,8 +3,9 @@ import unittest
 class test_one(unittest.TestCase):
     def test_actualtestone(self):
         self.assertEquals(True, True)
-        self.assertEquals(True, 0)
+    def test_actualtesttwo(self):
+        self.assertEquals(True, False)
 
-class test_one(unittest.TestCase):
+class test_two(unittest.TestCase):
     def test_panic(self):
         self.assertEquals(True, False)
