a348646006f75745edc0409b43e63f023dea5fdc
dbr <dbr.onix@gmail.com>
2008-12-30 00:51:16 +1030

    Removed unused network-growl stuff from setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = "pyautotest",
-    py_modules = ['pyautotest', 'rawr', 'netgrowl'],
+    py_modules = ['pyautotest'],
     entry_points = {
         'console_scripts': [
             'pyautotest = pyautotest:main'
