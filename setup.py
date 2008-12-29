12030d0b27d5e17fe49fff59d7d148f9c745347d
dbr <dbr.onix@gmail.com>
2008-12-30 00:58:06 +1030

    Missing ,

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = "pyautotest",
-    version = "1.0"
+    version = "1.0",
     py_modules = ['pyautotest'],
     entry_points = {
         'console_scripts': [
